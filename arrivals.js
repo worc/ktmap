b35cf265794dc0928fce286b43d9ae6b1c25b2c7
worc <curtis.s.mcallister@gmail.com>
2018-09-22 16:08:13 -0700

    stub out getting arrivals for a stop id

--- arrivals.js
+++ arrivals.js
@@ -1,3 +1,35 @@
+import React from 'react'
 import axios from 'axios'
 
-    
+export default class Arrivals extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            nextArrivals: []
+        }
+    }
+
+    componentDidMount() {
+        const appId = '5C3A497B4A51A9E15E3D97D4A'
+        const stopId = this.props.match.params.stopId
+        axios({
+            method: 'GET',
+            url: `https://developer.trimet.org/ws/v2/arrivals?locIDs=${ stopId }&appId=${ appId }`
+        }).then(response => {
+            console.log(response)
+            this.setState({ nextArrivals: response.data.resultSet.arrival })
+        })
+    }
+
+    render() {
+        const stopId = this.props.match.params.stopId
+
+        return (
+            <div>
+                <h1>ARRIVALS!</h1>
+                <div>{ stopId }</div>
+                { this.state.nextArrivals.map(arrival => <div key={ arrival.tripID }>{ arrival.shortSign }</div>) }
+            </div>
+        )
+    }
+}
--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
+import Nearby from './nearby'
+import Arrivals from './arrivals'
 import NotFound from './not_found'
 
 render(
     <BrowserRouter>
         <Switch>
+            <Route exact path='/arrivals' component={ Nearby }/>
+            <Route exact path='/arrivals/:stopId' component={ Arrivals }/>
             <Route path='*' component={ NotFound } />
         </Switch>
     </BrowserRouter>
