f67555bd3c330b377efa3572d798114d74e48617
worc <curtis.s.mcallister@gmail.com>
2018-09-22 18:15:48 -0700

    use hash routing

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom'
 
 import Nearby from './nearby'
 import Arrivals from './arrivals'
 import NotFound from './not_found'
 
 render(
-    <BrowserRouter>
+    <HashRouter>
         <Switch>
             <Redirect exact from='/' to='/arrivals' />
             <Route exact path='/arrivals' component={ Nearby }/>
             <Route exact path='/arrivals/:stopId' component={ Arrivals }/>
             <Route path='*' component={ NotFound } />
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
     , document.getElementById('app')
 )
