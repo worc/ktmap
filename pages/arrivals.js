7a0a736f34aceeaea4b69172424df2058a43f6cb
curtis mcallister <curtis.s.mcallister@gmail.com>
2019-04-09 16:44:44 -0700

    repeat stop information on the arrivals page

--- pages/arrivals.js
+++ pages/arrivals.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import axios from 'axios'
-
+import styled from 'styled-components'
 import Arrival from '../arrival'
 
 export default class Arrivals extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            nextArrivals: []
+            nextArrivals: [],
+            location: {},
         }
     }
 
@@ -19,19 +20,25 @@ export default class Arrivals extends React.Component {
             url: `https://developer.trimet.org/ws/v2/arrivals?locIDs=${ stopId }&appId=${ appId }`
         }).then(response => {
             console.log(response)
-            this.setState({ nextArrivals: response.data.resultSet.arrival })
+            this.setState({
+                nextArrivals: response.data.resultSet.arrival,
+                location: response.data.resultSet.location[0],
+            })
         })
     }
 
     render() {
-        const stopId = this.props.match.params.stopId
+        const location = this.state.location
 
         return (
-            <div>
-                <h1>ARRIVALS!</h1>
-                <div>{ stopId }</div>
-                { this.state.nextArrivals.map(arrival => <Arrival key={ arrival.tripID } arrival={ arrival } />) }
-            </div>
+            <ArrivalsContainer>
+                <h1>#{ location.id }, { location.desc }, { location.dir }</h1>
+                { this.state.nextArrivals.map(arrival => <Arrival key={ arrival.tripID } location={ this.state.location } arrival={ arrival } />) }
+            </ArrivalsContainer>
         )
     }
 }
+
+const ArrivalsContainer = styled.div`
+    margin-bottom: 15px;
+`
