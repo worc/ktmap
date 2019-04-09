e306876dcdaac4f57307db162f5e074f22d531af
curtis mcallister <curtis.s.mcallister@gmail.com>
2019-04-09 16:44:06 -0700

    declutter information about each arrival

--- arrival.js
+++ arrival.js
@@ -3,22 +3,24 @@ import styled from 'styled-components'
 import moment from 'moment'
 
 export default ({ arrival }) => {
-    const fields = Object.keys(arrival)
+    // const fields = Object.keys(arrival)
     const scheduled = moment(arrival.scheduled)
     const estimated = moment(arrival.estimated)
 
     return (
         <ArrivalGroup>
-            <h2>Route: { arrival.route }</h2>
-            <div>estimated arrival:</div>
-            <div>{ estimated.fromNow() } at { estimated.format('LT') }</div>
-            <div>scheduled arrival:</div>
-            <div>{ scheduled.fromNow() } at { scheduled.format('LT') }</div>
-            { fields.map(field => <div key={ field } >{ field }: { arrival[field] ? arrival[field].toString() : null }</div>) }
+            <h2>Route: { arrival.route }, { estimated.fromNow() } at { estimated.format('LT') }</h2>
+            <Scheduled>scheduled: { scheduled.fromNow() } at { scheduled.format('LT') }</Scheduled>
+            {/*{ fields.map(field => <div key={ field } >{ field }: { arrival[field] ? arrival[field].toString() : null }</div>) }*/}
         </ArrivalGroup>
     )
 }
 
 const ArrivalGroup = styled.div`
     margin-top: 10px;
+`
+
+const Scheduled = styled.div`
+    font-style: italic;
+    margin-top: -52px;
 `
