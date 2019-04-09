5ae283c966f426f215983912bdbf4c6abd0c53c6
curtis mcallister <curtis.s.mcallister@gmail.com>
2019-04-09 16:38:24 -0700

    headline each arrival with the route number, use the moment prefix so that times in the past are written as such

--- arrival.js
+++ arrival.js
@@ -6,14 +6,14 @@ export default ({ arrival }) => {
     const fields = Object.keys(arrival)
     const scheduled = moment(arrival.scheduled)
     const estimated = moment(arrival.estimated)
-    const omitPrefix = true
 
     return (
         <ArrivalGroup>
+            <h2>Route: { arrival.route }</h2>
             <div>estimated arrival:</div>
-            <div>{ estimated.fromNow(omitPrefix) } from now at { estimated.format('LT') }</div>
+            <div>{ estimated.fromNow() } at { estimated.format('LT') }</div>
             <div>scheduled arrival:</div>
-            <div>{ scheduled.fromNow(omitPrefix) } from now at { scheduled.format('LT') }</div>        
+            <div>{ scheduled.fromNow() } at { scheduled.format('LT') }</div>
             { fields.map(field => <div key={ field } >{ field }: { arrival[field] ? arrival[field].toString() : null }</div>) }
         </ArrivalGroup>
     )
