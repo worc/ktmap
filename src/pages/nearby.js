29f8ee32811316d4f7bb31b6fcef789a83e79494
worc <curtis.s.mcallister@gmail.com>
2019-10-10 00:29:35 -0700

    convert nearby component to a function component

--- src/pages/nearby.js
+++ src/pages/nearby.js
@@ -1,62 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 import Stop from '../stop'
 
+const appId = '5C3A497B4A51A9E15E3D97D4A'
+
+// maximum distance in feet to search for stops
+const distanceAway = 5280 / 2 // half mile
+
 // todo poll location, update if it's a significant enough difference from previous position
 // todo also eventually and time out anyways after 15 seconds or so
-export default class Nearby extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            userLocation: {
-                latitude: 0,
-                longitude: 0,
-            },
-            stops: []
-        }
-    }
+export default () => {
+    const [userLocation, setUserLocation] = useState({})
+    const [stops, setStops] = useState([])
 
-    componentDidMount() {
+    useEffect(() => {
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(position)
-            this.fetchStops({
+            setUserLocation({
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude,
-                distance: 5280 / 2 // half mile
-            })
-            const userLocation = {
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude
-            }
-            this.setState({ 
-                userLocation
             })
         })
-    }
+    }, [])
 
-    fetchStops({ latitude, longitude, distance }) {
-        const longLatQuery = `ll=${ longitude },${ latitude }`
-        const appId = '5C3A497B4A51A9E15E3D97D4A'
+    useEffect(() => {
+        if (userLocation.latitude && userLocation.longitude) {
+            const longLatQuery = `ll=${userLocation.longitude},${userLocation.latitude}`
+            axios({
+                method: 'GET',
+                url: `https://developer.trimet.org/ws/V1/stops?${ longLatQuery }&feet=${ distanceAway }&json=true&appId=${ appId }`,
+            }).then(response => {
+                setStops(response.data.resultSet.location)
+            })
+        }
+    },[userLocation])
 
-        axios({
-            method: 'GET',
-            url: `https://developer.trimet.org/ws/V1/stops?${ longLatQuery }&feet=${ distance }&json=true&appId=${ appId }`,
-        }).then(response => {
-            console.log(response)
-            this.setState({ stops: response.data.resultSet.location })
-        })
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval)
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                { this.state.stops.map(stop => <Stop key={ stop.locid } stop={ stop } userLocation={ this.state.userLocation } /> )}
-            </React.Fragment>
-        )
-    }
+    return (
+        <>
+            { stops.map(stop => <Stop key={ stop.locid } stop={ stop } userLocation={ userLocation } /> )}
+        </>
+    )
 }
