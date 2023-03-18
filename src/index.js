6cf5e1f959b1e762791c3dc074ef2bd5d6576d46
robert nasewytewa <robert.nasewytewa@gmail.com>
2023-03-17 23:01:44 -0700

    update to webpack 5, typescript boot up

--- src/index.js
+++ src/index.js
@@ -8,49 +8,49 @@ import Arrivals from './pages/arrivals'
 import NotFound from './not_found'
 
 const GlobalStyle = createGlobalStyle`
-    body {
-        background: #ccc;
-        color: #810e0e;
-        margin: 0;
-    }
+  body {
+    background: #ccc;
+    color: #810e0e;
+    margin: 0;
+  }
 
-    #app {
-        font-family: "Lato", sans-serif;
-        font-size: 16px;
-    }
-    
-    h1 {
-        font-size: 40px;
-    }
-    
-    h2 {
-        font-size: 32px;
-    }
+  #app {
+    font-family: "Lato", sans-serif;
+    font-size: 16px;
+  }
+
+  h1 {
+    font-size: 40px;
+  }
+
+  h2 {
+    font-size: 32px;
+  }
 `
 
 const MapOverlay = styled.div`
-    height: 100%;
-    width: 100%;
-    overflow: scroll;
-    padding: 8px;
-    position: fixed;
-    z-index: 0;
+  height: 100%;
+  width: 100%;
+  overflow: scroll;
+  padding: 8px;
+  position: fixed;
+  z-index: 0;
 `
 
 render(
-    <BrowserRouter>
-        <>
-            <GlobalStyle/>
-            <Map/>
-            <MapOverlay>
-                <Switch>
-                    <Redirect exact from='/' to='/arrivals' />
-                    <Route exact path='/arrivals' component={ Nearby }/>
-                    <Route exact path='/arrivals/:stopId' component={ Arrivals }/>
-                    <Route path='*' component={ NotFound } />
-                </Switch>
-            </MapOverlay>
-        </>
-    </BrowserRouter>
-    , document.getElementById('app')
+  <BrowserRouter>
+    <>
+      <GlobalStyle/>
+      <Map/>
+      <MapOverlay>
+        <Switch>
+          <Redirect exact from='/' to='/arrivals'/>
+          <Route exact path='/arrivals' component={Nearby}/>
+          <Route exact path='/arrivals/:stopId' component={Arrivals}/>
+          <Route path='*' component={NotFound}/>
+        </Switch>
+      </MapOverlay>
+    </>
+  </BrowserRouter>
+  , document.getElementById('app')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -5,56 +5,65 @@ const path = require('path')
 const NODE_ENV = process.env.NODE_ENV
 
 module.exports = {
-    mode: NODE_ENV,
+  mode: NODE_ENV,
 
-    devtool: NODE_ENV === 'production' ? 'source-map' : 'eval',
+  devtool: NODE_ENV === 'production' ? 'source-map' : 'eval',
 
-    devServer: {
-        contentBase: 'dist/',
-        historyApiFallback: true,
-    },
+  devServer: {
+    static: 'dist/',
+    historyApiFallback: true,
+  },
 
-    entry: {
-        main: path.resolve(__dirname, 'src/index.js'),
-    },
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js'),
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [ '@babel/preset-env', '@babel/preset-react' ],
-                    },
-                },
-            },
-            {
-                test: /\.md$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    }
-                ],
-            },
-        ]
-    },
+  resolve: {
+    extensions: [ '.ts', '.js', '.tsx', '.jsx'],
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.ejs',
-        }),
-        new BundleAnalyzerPlugin({
-            logLevel: 'error',
-            analyzerMode: 'static',
-            reportFilename: path.join(__dirname, `.bundle_analysis.html`),
-            openAnalyzer: false,
-        }),
-    ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.md$/,
+        use: [
+          {
+            loader: 'file-loader',
+          }
+        ],
+      },
+    ]
+  },
 
-    output: {
-        publicPath: '/',
-        filename: '[name].[hash].js',
-    },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.ejs',
+    }),
+    new BundleAnalyzerPlugin({
+      logLevel: 'error',
+      analyzerMode: 'static',
+      reportFilename: path.join(__dirname, `.bundle_analysis.html`),
+      openAnalyzer: false,
+    }),
+  ],
+
+  output: {
+    publicPath: '/',
+    filename: '[name].[hash].js',
+  },
 }
