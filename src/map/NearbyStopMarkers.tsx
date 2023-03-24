de27c9eca094d0e08016446c293e13ec381005c2
robert nasewytewa <robert.nasewytewa@gmail.com>
2023-03-24 01:22:55 -0700

    open popup when clicking on stops

--- src/map/NearbyStopMarkers.tsx
+++ src/map/NearbyStopMarkers.tsx
@@ -2,14 +2,21 @@ import React, { useContext } from 'react'
 import { Marker } from 'react-map-gl'
 import NearbyStops from '../context/NearbyStops'
 import Stop from '../components/Stop'
+import NextArrivals from '../context/NextArrivals'
 
 export default function NearbyStopMarkers () {
   const stops = useContext(NearbyStops)
+  const { setShow, setStopId } = useContext(NextArrivals)
+
+  function handleClick (stopId: number) {
+    setShow(true)
+    setStopId(stopId)
+  }
 
   return (
     <>
       { stops.map(stop => (
-        <Marker key={stop.locid} latitude={stop.lat} longitude={stop.lng} anchor="top">
+        <Marker key={stop.locid} latitude={stop.lat} longitude={stop.lng} anchor="top" onClick={() => handleClick(stop.locid)}>
           <Stop {...stop } />
         </Marker>
       ))}
