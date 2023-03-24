d0cdbafb9771917b2d0efb9a48be265d7f6b0f75
robert nasewytewa <robert.nasewytewa@gmail.com>
2023-03-23 17:24:40 -0700

    replace user location filtering with Kalman filter directly in the hook

--- src/hooks/useUserLocation.ts
+++ src/hooks/useUserLocation.ts
@@ -1,22 +1,60 @@
+// Tried to implement a Kalman filter in this hook: https://stackoverflow.com/a/15657798/769780
+
 import { useState, useEffect } from 'react'
 import PORTLANDIA from '../portlandia'
 
-export default (initialPosition = PORTLANDIA) => {
-  const [userLocation, setUserLocation] = useState(initialPosition)
+const MINIMUM_ACCURACY = 1
+const DECAY = 15 // in meters per second
 
-  function getCurrentPosition() {
-    navigator.geolocation.getCurrentPosition(position => {
-      setUserLocation({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-        unit: 'DEGREES',
-      })
+export default (initialPosition = PORTLANDIA) => {
+  const [lastTimestamp, setLastTimestamp] = useState(Date.now())
+  const [userLocation, setUserLocation] = useState(initialPosition)
+  const [variance, setVariance] = useState(1)
+  const [lastKnownHeading, setLastKnownHeading] = useState(0)
+
+  function setCurrentPosition (position: GeolocationPosition) {
+    const now = Date.now()
+    const timeElapsed = now - lastTimestamp
+    const currentVariance = variance + (timeElapsed * DECAY * DECAY) / 1000
+    const accuracy = Math.max(userLocation.accuracy ?? 0, MINIMUM_ACCURACY)
+
+    const K = currentVariance / (currentVariance + accuracy * accuracy)
+    const newVariance = (1 - K) * currentVariance
+
+    const latitude = userLocation.latitude + K * (position.coords.latitude - userLocation.latitude)
+    const longitude = userLocation.longitude + K * (position.coords.longitude - userLocation.longitude)
+
+    const speed = position.coords.speed
+    const heading = position.coords.heading ?? lastKnownHeading
+
+    setLastTimestamp(now)
+
+    setVariance(newVariance)
+
+    setLastKnownHeading(heading)
+
+    setUserLocation({
+      ...userLocation,
+      accuracy: position.coords.accuracy,
+      altitude: position.coords.altitude,
+      heading,
+      latitude,
+      longitude,
+      unit: 'DEGREES',
     })
   }
 
+  function handleError (error: GeolocationPositionError) {
+    console.error(error.message)
+  }
+
   useEffect(() => {
-    const interval = setInterval(getCurrentPosition, 5000)
-    return () => clearInterval(interval)
+    const watch = navigator.geolocation.watchPosition(setCurrentPosition, handleError, {
+      maximumAge: 1000,
+      enableHighAccuracy: true,
+    })
+
+    return () => navigator.geolocation.clearWatch(watch)
   }, [])
 
   return userLocation
--- src/map/UserMarker.tsx
+++ src/map/UserMarker.tsx
@@ -6,16 +6,35 @@ import UserLocation from '../context/UserLocation'
 export default function UserMarker () {
   const userLocation = useContext(UserLocation)
 
+  console.log(userLocation.heading)
+
   return (
     <Marker key="user-location" latitude={userLocation.latitude} longitude={userLocation.longitude} anchor="bottom">
-      <User/>
+      <User heading={userLocation.heading}/>
     </Marker>
   )
 }
 
-const User = styled.div`
-  height: 25px;
-  width: 25px;
-  border: 4px solid blue;
-  border-radius: 50%;
+interface User {
+  readonly heading: number | null,
+}
+
+const User = styled.div<User>`
+  //height: 25px;
+  //width: 25px;
+  //border: 4px solid blue;
+  //border-radius: 50%;
+
+  width: 0;
+  height: 0;
+  border-left: 10px solid transparent;
+  border-right: 10px solid transparent;
+
+  border-bottom: 30px solid blue;
+  
+  transform: rotate(${props => props.heading ?? 0}deg);
 `
+
+// User.defaultProps = {
+//   bearing: 0
+// }
